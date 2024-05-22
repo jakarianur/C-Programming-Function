414a40e517f13bc4897007e393391d0dbce4c0db
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-22 22:10:30 +0600

    Update 03. Write a program to find out the maximum or minimum element of an array using a user defined function.c

--- 03. Write a program to find out the maximum or minimum element of an array using a user defined function.c
+++ 03. Write a program to find out the maximum or minimum element of an array using a user defined function.c
@@ -1,1 +1,32 @@
-
+#include <stdio.h>
+int findMax(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];}
+    }
+    return max;
+}
+int findMin(int arr[], int n) {
+    int min = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+}int main() {
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+int arr[n];
+printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }int max = findMax(arr, n);
+    printf("The maximum element in the array is: %d\n", max);
+int min = findMin(arr, n);
+    printf("The minimum element in the array is: %d\n", min);
+return 0;
+}
