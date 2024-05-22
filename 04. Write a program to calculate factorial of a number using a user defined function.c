5f6e4a7f46e7cf4e16492069cf758e85acc1a0dd
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-22 22:35:46 +0600

    Update 04. Write a program to calculate factorial of a number using a user defined function.c

--- 04. Write a program to calculate factorial of a number using a user defined function.c
+++ 04. Write a program to calculate factorial of a number using a user defined function.c
@@ -1,33 +1,16 @@
 #include<stdio.h>
-
-int factorial(int num1) {
-    int i, fac = 1;
-    for (i = 1; i <= num1; i++) {
-        fac *= i;
+void fact()
+{
+    int num , i, result=1;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    for(i=1; i<=num; i++)
+    {
+        result*=i;
     }
-    return fac;
-}#include <stdio.h>
-#include <string.h>
-
-int isPalindrome(char str1[]) {
-    char str2[1000];
-    strcpy(str2, str1);
-    strrev(str2);
-    int res = strcmp(str1, str2);
-    return res == 0; // Returns 1 if strings are equal, indicating a palindrome
+    printf("Factorial is %d \n", result);
 }
-
 int main() {
-    char str1[1000];
-
-    printf("Enter a String: ");
-    fgets(str1, sizeof(str1), stdin);
-
-    // Remove newline character from input
-    str1[strcspn(str1, "\n")] = '\0';
-
-    if (isPalindrome(str1)) {
-        printf("This is Palindrome.\n");
-    } else {
-        printf("This is not Palindrome.\n");
-    }
+    fact();
+    return 0;
+}
