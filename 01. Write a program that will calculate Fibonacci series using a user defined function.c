94822ff2f5edbd2355e1af0ba826b34d94d0f64f
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-22 21:50:24 +0600

    Update 01. Write a program that will calculate Fibonacci series using a user defined function.c

--- 01. Write a program that will calculate Fibonacci series using a user defined function.c
+++ 01. Write a program that will calculate Fibonacci series using a user defined function.c
@@ -1,1 +1,21 @@
-
+#include <stdio.h>
+void fib(int num)
+{
+    int t1=0, t2=0, nextTerm;
+    printf("Fibonacci series: %d %d", t1,t2);
+    for (int i=3; i<=num; i++)
+    {
+        nextTerm=t1+t2;
+        printf(", %d",nextTerm);
+        t1=t2;
+        t2=nextTerm;
+    }
+    printf("\n");
+}
+int main() {
+    int n;
+    printf("Enter the numbers of terms: ");
+    scanf("%d",&n);
+    fib(n);
+    return 0;
+}
