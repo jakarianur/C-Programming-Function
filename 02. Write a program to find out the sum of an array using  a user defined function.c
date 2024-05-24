8908a83b3968b06652153069004e0a4825531264
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-24 23:50:04 +0600

    Update 02. Write a program to find out the sum of an array using  a user defined function.c

--- 02. Write a program to find out the sum of an array using  a user defined function.c
+++ 02. Write a program to find out the sum of an array using  a user defined function.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-int sumOfArray(int arr[], int size) {
+int sumOfArray(int arr[], int size)
+{
     int sum = 0;
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         sum += arr[i];
     }
     return sum;
 }
-int main() {
+int main()
+{
     int arr[] = {1, 2, 3, 4, 5};
     int n = sizeof(arr) / sizeof(arr[0]); 
     int totalSum = sumOfArray(arr, n);
